6eb83b76fa0a12b196db498571caffcbfad1819f
Carlos Esteban Raffellini <carlosraffellini@gmail.com>
2017-07-12 14:37:42 +0100

    extract how to read files and rename variables

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ var app = express()
 
 app.set('view engine', 'pug')
 
+var sourceDirectory = "./files";
+
+function getFiles(directory){
+    return fs.readdirSync(directory);
+}
 
 app.get('/', function (req, res) {
-    var files = fs.readdirSync("./files");
+    var files = getFiles(sourceDirectory);
     
-    res.render("index", {message: files});
+    res.render("index", {files: files});
 })
 
 app.listen(8080);
