97b08ddf2eb3dbcf10b6ee318e83d42e8273bf8c
Carlos Esteban Raffellini <carlosraffellini@gmail.com>
2017-07-12 14:50:28 +0100

    create file repository

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-var express = require('express')
-var fs = require("fs");
-var app = express()
+var express = require('express');
+var FileRepository = require('./src/fileRepository');
+var app = express();
 
 app.set('view engine', 'pug')
 
 var sourceDirectory = "./files";
-
-function getFiles(directory){
-    return fs.readdirSync(directory);
-}
+var repo = new FileRepository(sourceDirectory);
 
 app.get('/', function (req, res) {
-    var files = getFiles(sourceDirectory);
+    var files = repo.getFiles();
     
     res.render("index", {files: files});
 })
